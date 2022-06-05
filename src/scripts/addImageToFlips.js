af3fab3a10e821f7077dc62bd07182bcfec02ccb
susuwatarii <aidangomar@gmail.com>
2022-06-05 00:45:01 -0400

    shit may be about to go haywire with webpack

--- src/scripts/addImageToFlips.js
+++ src/scripts/addImageToFlips.js
@@ -1,5 +1,15 @@
-const flipped_nfts = require('../data/flipped_nfts.json')
+/* TODO:
+    so a lot of this should probably be wrapped in a try/catch because
+    when using the contract abi, there's a very good chance for some contracts
+    that the tokenURI won't be able to render the image, so handle errs
+    accordingly and init vals with null
+    
 
+*/
+
+
+
+const flipped_nfts = require('../data/flipped_nfts.json')
 
 const addImage = () => {
 
